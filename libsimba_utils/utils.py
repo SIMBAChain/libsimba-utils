5dc16d876c90a9b41c1ce0d65f38b80453e2b664
Andrew Harrison <doctor.zob@gmail.com>
2022-09-14 15:07:26 +0100

    [NO_ISSUE] string to uint utility

--- libsimba_utils/utils.py
+++ libsimba_utils/utils.py
@@ -4,8 +4,18 @@ from Crypto.Hash import keccak
 
 
 def convert_to_bytes32_array(
-    string: str, length: int, extract_single_array: bool = True
+    string: str, length: int, extract_single_array: bool = False, prefix: bool = True
 ) -> Union[List, str]:
+    """
+    Convert a string to an aray of byte32 arrays in hex representation.
+    The string will be truncated if it does not fit into the arrays (32 bytes * length)
+    :param string: the string to convert
+    :param length: the length of the array to return.
+    :param extract_single_array: If a single length array, whether to return just the bytes, i.e.,
+           a single byte32 array.
+    :param prefix: whether or not to prefix the hex strings with '0x'.
+    :return: Either an array of byte arrays or a single byte array.
+    """
     byte_array = bytearray(32 * length)
     byte_val = bytearray(string, "utf8")
     byte_array[0 : len(byte_val)] = byte_val
@@ -13,27 +23,66 @@ def convert_to_bytes32_array(
     for i in range(0, length):
         part = bytearray(32)
         part[:] = byte_array[32 * i : 32 + 32 * i]
-        bytes32_arr.append("0x" + part.hex())
+        pref = "0x" if prefix else ""
+        bytes32_arr.append(f"{pref}{part.hex()}")
     if len(bytes32_arr) == 1 and extract_single_array:
         return bytes32_arr[0]
     return bytes32_arr
 
 
 def convert_bytes32_to_string(
-    bytes32_array: Union[List, Tuple, str], length: int = 1
+    bytes32_array: Union[List, Tuple, str], length: int = None
 ) -> str:
+    """
+    Convert a bytes32 in hex representation or an array of
+    bytes32 in hex representation to a string.
+    The representations are assumed to
+    :param bytes32_array: the array of bytes or byte arrays
+    :param length: The length of the array to convert if input is an array.
+           If none, defaults to the length of the input array.
+    :return: a string
+    """
     if isinstance(bytes32_array, (list, tuple)):
+        if length is None:
+            length = len(bytes32_array)
         byte_array = bytearray(32 * length)
         for i in range(0, length):
-            chunk = bytearray.fromhex(bytes32_array[i][2:])
+            offset = 2 if bytes32_array[i].startswith("0x") else 0
+            chunk = bytearray.fromhex(bytes32_array[i][offset:])
             byte_array[i * 32 :] = chunk
         bs = byte_array.decode("utf8")
-        return bs.strip("\u0000")
     else:
-        chunk = bytearray.fromhex(bytes32_array[2:])
+        offset = 2 if bytes32_array.startswith("0x") else 0
+        chunk = bytearray.fromhex(bytes32_array[offset:])
         bs = chunk.decode("utf8")
-        return bs.strip("\u0000")
+    return bs.strip("\u0000")
 
 
-def keccak_hash(value: str, bits: int = 256):
-    return keccak.new(digest_bits=bits).update(value.encode("utf-8")).hexdigest()
+def keccak_hash(value: str, bits: int = 256, as_hex: bool = True) -> Union[str, bytes]:
+    """
+    HAsh a value.
+    :param value: the value to hash
+    :param bits: the numver of bits - default is 256
+    :param as_hex: whether to return a hex digest or the raw bytes
+    :return:
+    """
+    k_hash = keccak.new(digest_bits=bits).update(value.encode("utf-8"))
+    if as_hex:
+        return k_hash.hexdigest()
+    else:
+        return k_hash.digest()
+
+
+def string_to_uint256(value: str, big_endian: bool = True) -> int:
+    """
+    Convert any length string to an unsigned 256 bit integer via hashing
+    :param value: The string value to convert
+    :param big_endian: whether to use big or little endian byte order. Solidity keccak uses big.
+    :return: an integer
+    """
+    hash_val = keccak_hash(value, as_hex=False)
+    return (
+        int.from_bytes(hash_val, byteorder="big", signed=False)
+        if big_endian
+        else (int.from_bytes(hash_val, byteorder="little", signed=False))
+    )
--- tests/test_deser.py
+++ tests/test_deser.py
@@ -1,5 +1,5 @@
 import unittest
-from libsimba_utils.utils import convert_bytes32_to_string, convert_to_bytes32_array
+from libsimba_utils.utils import convert_bytes32_to_string, convert_to_bytes32_array, keccak_hash, string_to_uint256
 
 
 class TestSer(unittest.TestCase):
@@ -23,9 +23,47 @@ class TestSer(unittest.TestCase):
         }
 
         outputs = {
-            "name": convert_bytes32_to_string(inputs["name"], 1),
-            "description": convert_bytes32_to_string(inputs["description"], 4),
+            "name": convert_bytes32_to_string(inputs["name"]),
+            "description": convert_bytes32_to_string(inputs["description"]),
             "image": convert_bytes32_to_string(inputs["image"], 4),
             "contentHash": inputs["contentHash"]
         }
         assert data == outputs
+
+        inputs_no_pref = {
+            "name": convert_to_bytes32_array(data["name"], 1, prefix=False),
+            "description": convert_to_bytes32_array(data["description"], 4, prefix=False),
+            "image": convert_to_bytes32_array(data["image"], 4, prefix=False),
+            "contentHash": data["contentHash"]
+        }
+
+        outputs_no_pref = {
+            "name": convert_bytes32_to_string(inputs_no_pref["name"]),
+            "description": convert_bytes32_to_string(inputs_no_pref["description"]),
+            "image": convert_bytes32_to_string(inputs_no_pref["image"]),
+            "contentHash": inputs["contentHash"]
+        }
+        assert data == outputs_no_pref
+
+        # truncated
+        description = convert_bytes32_to_string(inputs["description"], 2)
+        assert description == "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+        assert len(description) == 64
+
+    def test_conversions(self):
+        hash_val = keccak_hash("A short string")
+        assert hash_val == "c02616352442fd8d6b29e47623743b0644e7168b988c4545b71df141245db363"
+        int_val = int.from_bytes(bytes.fromhex(hash_val), byteorder="big", signed=False)
+        assert int_val == 86911360387564328691877682441780275511611438275732502394544537523081168401251
+        recovered = int_val.to_bytes(32, byteorder='big')
+        hexed = recovered.hex()
+        assert hexed == hash_val
+        int_val2 = string_to_uint256("A short string")
+        #  function convert(string memory value) external pure returns (uint256) {
+        #      bytes32 hash = keccak256(bytes(value));
+        #      return uint256(hash);
+        #  }
+        #  86911360387564328691877682441780275511611438275732502394544537523081168401251
+        assert int_val2 == int_val
+        int_val3 = string_to_uint256(f"A {' '.join(['very' for i in range(100)])}long string")
+        assert int_val3 == 44831467989385379626249444820128067825371402133342580492608562458279217187427
